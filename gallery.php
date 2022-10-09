14a6c4c9c6b7f5a2385006a8ca210f097c096ca1
abdullah-al-masum-24 <115403587+abdullah-al-masum-24@users.noreply.github.com>
2022-10-09 23:56:37 +0600

    project-uploaded
    project-uploaded

---
+++ gallery.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gallery</title>
+</head>
+<body bgcolor="#282a35">
+<table border="2" align="center" bgcolor="#f4faf6" width="1000">
+    <tr>
+        <td height="200" width="200"><img height="200" width="200" src="./assets/img/img_1.png" alt="logo"></td>
+        <td bgcolor="#f6f6f6"></td>
+    </tr>
+    <tr>
+        <td height="50" colspan="2">
+            <table border="1" width="1000">
+                <tr align="center" bgcolor="#04aa6d">
+                    <td height="50" ><a href="index.php" target="_blank">Home</a></td>
+                    <td><a href="contact.php" target="_blank">Contact</a></td>
+                    <td><a href="portfolio.php" target="_blank">Portfolio</a></td>
+                    <td><a href="about.php" target="_blank">About Us</a></td>
+                    <td><a href="gallery.php" target="_parent">Gallery</a></td>
+                </tr>
+
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td height="600" bgcolor="#f6f6f6"></td>
+        <td align="center">
+            <h3>Gallery</h3>
+            <br/>
+            <table align="center" border>
+                <tr>
+                    <td height="200" width="200" ><img height="200" width="200"  src="./assets/img/gallery/img_1.jpg" alt=""></td>
+                    <td height="200" width="200" ><img height="200" width="200"  src="./assets/img/gallery/img_2.jpg" alt=""></td>
+                    <td height="200" width="200" ><img height="200" width="200"  src="./assets/img/gallery/img_3.jpg" alt=""></td>
+                </tr>
+                <tr>
+                    <td height="200"  ><img height="200" width="200"  src="./assets/img/gallery/img_4.jpg" alt=""></td>
+                    <td height="200"  ><img height="200" width="200"  src="./assets/img/gallery/img_5.jpg" alt=""></td>
+                    <td height="200"  ><img height="200" width="200"  src="./assets/img/gallery/img_6.jpg" alt=""></td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td height="250" colspan="2" bgcolor="#d3d3d3">
+            <table border="1"  width="1000" >
+                <tr align="center">
+                    <td height="250" width="380">
+                        <h3 >About Me</h3>
+                        <hr/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis consectetur fugiat fugit hic itaque iusto nulla rem reprehenderit! Ad commodi deserunt dignissimos dolorem doloribus, eum fugit maxime neque perspiciatis tempora?</p>
+                    </td>
+                    <td>
+                        <h3 >Popular Services</h3>
+                        <hr/>
+                        <ul>
+                            <li><a href="">Ecommerce Development</a></li>
+                            <li><a href="">News Portal Development</a></li>
+                            <li><a href="">Company Website Development</a></li>
+                            <li><a href="">Customized Website Development</a></li>
+                        </ul>
+                    </td>
+                    <td>
+                        <h3 >Emergency Contact</h3>
+                        <hr/>
+                        <address>
+                            Mrpur, Section-12, Block-D, Dhaka-1200.<br/>
+                            <b>Phone :</b>01719317197, <br/>
+                            <b>Email :</b>cacrmdabdullah@gmail.com, <br/>
+                            <b>Facebook :</b>facebook.com/mdmamun, <br/>
+                            <b>Twitter :</b>twiter.com/mdmamun.
+                        </address>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2" height="50" bgcolor="#04aa6d" align="center">
+            <div>
+                &copy; copyright <strong><a href="">Abdullah Al Mamun</a></strong> . All Rights Reserved.
+            </div>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
---
+++ portfolio.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio</title>
+</head>
+<body bgcolor="#282a35">
+<table border="2" align="center" bgcolor="#f4faf6" width="1000">
+    <tr>
+        <td height="200" width="200"><img height="200" width="200" src="./assets/img/img_1.png" alt="logo"></td>
+        <td bgcolor="#f6f6f6"></td>
+    </tr>
+    <tr>
+        <td height="50" colspan="2">
+            <table border="1" width="1000">
+                <tr align="center" bgcolor="#04aa6d">
+                    <td height="50" ><a href="index.php" target="_blank">Home</a></td>
+                    <td><a href="contact.php" target="_blank">Contact</a></td>
+                    <td><a href="portfolio.php" target="_parent">Portfolio</a></td>
+                    <td><a href="about.php" target="_blank">About Us</a></td>
+                    <td><a href="gallery.php" target="_blank">Gallery</a></td>
+                </tr>
+
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td height="600" bgcolor="#f6f6f6"></td>
+        <td valign="top" align="center">
+            <h3>RESUME</h3>
+            <h3>OF</h3>
+            <h3>ABDULLAH AL MAMUN</h3>
+            <br/>
+            <table align="center" width="700" border="1" bgcolor="#f4faf6" >
+                <tr>
+                    <td width="500" height="200">
+                        <h3>MALING ADDRESS</h3>
+                        <address>
+                            <strong>ABDULLAH AL MAMUN </strong><br/>
+                            <b>C/O-</b>Md Nurezzaman <br/>
+                            <b>Vill:</b> Fakirpara <br/>
+                            <b>P.S:</b>Kotowali, Dist: Dinajpur <br/>
+                            <b>Mobile:</b> +01719317197 <br/>
+                            <b>E-Mail:</b> cscrmdabdullah@gmail.com
+                        </address>
+                    </td>
+                    <td>
+                        <img height="200" width="200" src="./assets/img/img_2.jpg" alt="">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <h3>OBJECTIVE</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto atque commodi consequatur dolorem eius est exercitationem facilis hic ipsam itaque, necessitatibus nesciunt obcaecati pariatur praesentium quasi quis quo quos ratione repudiandae sapiente soluta tenetur totam. Accusantium cupiditate fugiat iure, iusto libero, mollitia nam necessitatibus nobis numquam ratione tenetur ullam.</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                         <h3>EDUCATIONAL QUALIFICATION</h3>
+                         <ul>
+                             <li>
+                                 <h3><ins>Bchalor of Science (B.Sc) Hon's:</ins></h3>
+                                 <p>Name of the Institute :  Delta Computer Science and Engineering College</p>
+                                 <p>Subject               :  Computer Science and Engineering</p>
+                                 <p>Session               :  2017/2018</p>
+                                 <p>Present Study Status  :  4<sup>th</sup> Year 7<sup>th</sup> Semester</p>
+                             </li>
+                         </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <h3>OBJECTIVE</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto atque commodi consequatur dolorem eius est exercitationem facilis hic ipsam itaque, necessitatibus nesciunt obcaecati pariatur praesentium quasi quis quo quos ratione repudiandae sapiente soluta tenetur totam. Accusantium cupiditate fugiat iure, iusto libero, mollitia nam necessitatibus nobis numquam ratione tenetur ullam.</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Signature
+                        <br/><br/>
+                        Abdullah Al Mamun
+                        <br/><br/>
+                    </td>
+                    <td align="center">
+                        Date:10/7/2022
+                    </td>
+                </tr>
+            </table>
+            <br/>
+        </td>
+    </tr>
+    <tr>
+        <td height="250" colspan="2" bgcolor="#d3d3d3">
+            <table border="1"  width="1000" >
+                <tr align="center">
+                    <td height="250" width="380">
+                        <h3 >About Me</h3>
+                        <hr/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis consectetur fugiat fugit hic itaque iusto nulla rem reprehenderit! Ad commodi deserunt dignissimos dolorem doloribus, eum fugit maxime neque perspiciatis tempora?</p>
+                    </td>
+                    <td>
+                        <h3 >Popular Services</h3>
+                        <hr/>
+                        <ul>
+                            <li><a href="">Ecommerce Development</a></li>
+                            <li><a href="">News Portal Development</a></li>
+                            <li><a href="">Company Website Development</a></li>
+                            <li><a href="">Customized Website Development</a></li>
+                        </ul>
+                    </td>
+                    <td>
+                        <h3 >Emergency Contact</h3>
+                        <hr/>
+                        <address>
+                            Mrpur, Section-12, Block-D, Dhaka-1200.<br/>
+                            <b>Phone :</b>01719317197, <br/>
+                            <b>Email :</b>cacrmdabdullah@gmail.com, <br/>
+                            <b>Facebook :</b>facebook.com/mdmamun, <br/>
+                            <b>Twitter :</b>twiter.com/mdmamun.
+                        </address>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2" height="50" bgcolor="#04aa6d" align="center">
+            <div>
+                &copy; copyright <strong><a href="">Abdullah Al Mamun</a></strong> . All Rights Reserved.
+            </div>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
